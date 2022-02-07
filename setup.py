25ff8f2f8153b4e086189e41d8618ef0e8b71024
Eli Rykoff <erykoff@stanford.edu>
2022-02-07 09:20:50 -0800

    Change to setuptools_scm_git_archive for versioning.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-exec(open('decasu/_version.py').read())
 
 name = 'decasu'
 
@@ -10,10 +9,11 @@ scripts = ['scripts/decasu_hpix_mapper.py',
 setup(
     name=name,
     packages=find_packages(exclude=('tests')),
-    version=__version__, # noqa
     description='DECam Survey Property Maps using healsparse',
     author='Eli Rykoff and others',
     author_email='erykoff@stanford.edu',
     url='https://github.com/erykoff/decasu',
     scripts=scripts,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
 )
