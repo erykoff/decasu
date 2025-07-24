cacb6a189ac6f68e013ad17b4c3e1ea478dd2c57
Eli Rykoff <erykoff@stanford.edu>
2025-07-24 12:09:28 -0700

    Add more command-line configuration overrides.

--- decasu/decasu_hpix_mapper.py
+++ decasu/decasu_hpix_mapper.py
@@ -11,23 +11,33 @@ def main():
     parser.add_argument('-c', '--configfile', action='store', type=str, required=True,
                         help='YAML config file')
     parser.add_argument('-i', '--infile', action='store', type=str, required=True,
-                        help='Input fits or database file')
+                        help='Input fits or database file or database connection string')
     parser.add_argument('-b', '--bands', action='store', type=str, required=False,
                         help='Bands to generate map for, comma delimited')
     parser.add_argument('-n', '--ncores', action='store', type=int, required=False,
                         default=1, help='Number of cores to run on.')
     parser.add_argument('-o', '--outputpath', action='store', type=str, required=True,
                         help='Output path')
+    parser.add_argument('-b', '--outputbase', action='store', type=str, required=False,
+                        help='Output filename base; will replace outbase in config.')
     parser.add_argument('-p', '--pixels', action='store', type=str, required=False,
                         help='Pixels to run on, comma delimited')
     parser.add_argument('-s', '--simple', action='store_true', required=False,
                         help='Run in simple mode (nexp only)')
     parser.add_argument('-k', '--keep_intermediate_files', action='store_true',
                         required=False, help='Keep intermediate files')
+    parser.add_argument('-q', '--query', required=False,
+                        help='Additional query string; will replace lsst_db_additional_selection config.')
 
     args = parser.parse_args()
 
     config = Configuration.load_yaml(args.configfile)
+
+    if args.outputbase is not None:
+        config.outbase = args.outputbase
+
+    if args.query is not None:
+        config.lsst_db_additional_selection = args.query
 
     if args.bands is None:
         bands = []
